cde007971f544b4393c47343c65cd2995a7765a8
TAO Software <taointeraktiv@gmail.com>
2021-07-05 16:06:49 +0200

    gradle file updated

---
+++ ios/Pods/Target Support Files/Pods-festival_app/Pods-festival_app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_festival_app : NSObject
+@end
+@implementation PodsDummy_Pods_festival_app
+@end
---
+++ ios/Pods/Target Support Files/PushyRN/PushyRN-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PushyRN : NSObject
+@end
+@implementation PodsDummy_PushyRN
+@end
